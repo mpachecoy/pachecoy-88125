8bf7ee98276d3775106ccab219165f2bbd0b9dc8
mpachecoy <ing.pachecoymartin@gmail.com>
2025-09-23 12:11:33 -0300

    Agregando interaccion

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const usuario = [];
 const turnos = [];
 
-let condicion = true;
+let condicion = confirm("Bienvenido!, dale aceptar si no sos usuario");
 
 while (condicion) {
   function createUser() {
@@ -12,11 +12,26 @@ while (condicion) {
     if (nombre != "" && age != "" && obraSocial != "") {
       usuario.push(nombre, age, obraSocial);
       console.log(usuario);
-      alert(`Bienvenido ${nombre}`)
-      condicion = false
+      alert(`Bienvenido ${nombre}`);
     } else {
       alert("Ingrese los datos requeridos");
     }
+    condicion = false;
   }
   createUser();
 }
+
+function pedirTurno() {
+  let doctor = prompt("Ingreste con quien quiere el turno");
+  let fecha = prompt("Ingrese la fecha");
+
+  if (doctor != "" && fecha != "") {
+    turnos.push(doctor, fecha);
+    console.log(turnos);
+    alert(`Su turno fue agendado correctamente`);
+  } else {
+    alert("Ingrese los datos requeridos");
+  }
+}
+
+pedirTurno();
