6171504c0c8490310ea08fc41de680ab1ee1f128
mpachecoy <ing.pachecoymartin@gmail.com>
2025-09-23 19:31:07 -0300

    Guardar usuarios en objetos

--- js/main.js
+++ js/main.js
@@ -4,34 +4,43 @@ const turnos = [];
 let condicion = confirm("Bienvenido!, dale aceptar si no sos usuario");
 
 while (condicion) {
-  function createUser() {
-    let nombre = prompt("Ingrese nombre");
-    let age = parseInt(prompt("Ingrese la edad"));
-    let obraSocial = prompt("Ingrese su obra social");
+    function createUser() {
+        let nombre = prompt("Ingrese nombre");
+        let edad = parseInt(prompt("Ingrese la edad"));
+        let obraSocial = prompt("Ingrese su obra social");
 
-    if (nombre != "" && age != "" && obraSocial != "") {
-      usuario.push(nombre, age, obraSocial);
-      console.log(usuario);
-      alert(`Bienvenido ${nombre}`);
-    } else {
-      alert("Ingrese los datos requeridos");
+        if (nombre != "" && edad != "" && obraSocial != "") {
+            let nuevoUsuario = {
+                nombre: nombre,
+                edad: edad,
+                obraSocial: obraSocial
+            }
+            usuario.push(nuevoUsuario)
+            console.log(usuario);
+            alert(`Bienvenido ${nombre}`);
+        } else {
+            alert("Ingrese los datos requeridos");
+        }
+        condicion = false;
     }
-    condicion = false;
-  }
-  createUser();
+    createUser();
 }
 
 function pedirTurno() {
-  let doctor = prompt("Ingreste con quien quiere el turno");
-  let fecha = prompt("Ingrese la fecha");
+    let doctor = prompt("Ingreste con quien quiere el turno");
+    let fecha = prompt("Ingrese la fecha");
 
-  if (doctor != "" && fecha != "") {
-    turnos.push(doctor, fecha);
-    console.log(turnos);
-    alert(`Su turno fue agendado correctamente`);
-  } else {
-    alert("Ingrese los datos requeridos");
-  }
+    if (doctor != "" && fecha != "") {
+        let nuevoTurno = {
+            doctor: doctor,
+            fecha: fecha
+        }
+        turnos.push(nuevoTurno);
+        console.log(turnos);
+        alert(`Su turno fue agendado correctamente`);
+    } else {
+        alert("Ingrese los datos requeridos");
+    }
 }
 
 pedirTurno();
